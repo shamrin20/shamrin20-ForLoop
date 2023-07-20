41260040af961f948fd94a1dc6a21e54794a2974
jhigherevature <71463449+jhigherevature@users.noreply.github.com>
2023-07-20 09:07:21 -0600

    Update Lab.java

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -37,7 +37,7 @@ public class Lab {
      *
      *      @param start the value of i where the for loop should start.
      *      @param end the value of i where the for loop should end (provided that the for loop is incremented with i++)
-     *      @return the sum of all numbers between start and end, with start inclusive and end exclusive.
+     *      @return the string of all numbers between start and end, back-to-back. The string is inclusive of start and exclusive of end.
      */
     public String returnNumbers(int start, int end){
         String buildingString = "";
